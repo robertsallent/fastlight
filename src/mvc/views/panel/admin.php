7f95f6de5e6469de834e5396fb713f715872bbb4
Robert Sallent <robertsallent@gmail.com>
2025-09-18 13:28:15 +0200

    v2.0.4c

--- src/mvc/views/panel/admin.php
+++ src/mvc/views/panel/admin.php
@@ -28,11 +28,10 @@
     		<p>A continuación se muestran los enlaces a las distintas operaciones 
     		   de administración de la aplicación <b><?= APP_NAME ?></b>.</p>
     		
-    		<div class="flex-container gap2">
-        		
-        		
+    		
+        	<div class="flex-container gap2">
         		<section class="flex1">
-        			<h2>Administración</h2>
+        			<h2 class="my2">Administración</h2>
         			<ul class="my2">
         				<?php if((Login::oneRole(ERROR_ROLES)) && (DB_ERRORS || LOG_ERRORS || LOG_LOGIN_ERRORS)){ ?>
         				<li><a href='/Error/list'>Errores</a></li>
@@ -46,23 +45,71 @@
            				<li><a href='/Test'>Test</a></li>
 						<?php } ?>
         			</ul>
-        			<p>Se muestran operaciones a las que tiene acceso el usuario administrador, siempre
-        			y cuando se le haya dado permiso en el fichero de configuración.</p>
+        			
         		</section>
         		
         		<?php if(Login::isAdmin()) {?>
         		<section class="flex1">
-        			<h2>Gestión de usuarios</h2>
+        			<h2 class="my2">Gestión de usuarios</h2>
     
         			<ul class="my2">
-        				<li><a href='/User'>Lista de usuarios</a> (TODO)</li>
-        				<li><a href='/User/create'>Nuevo usuario</a> (TODO)</li>
-        				<li>...</li>
+        				<li><a href='/User'>Lista de usuarios</a> (se implementa en clase)</li>
+        				<li><a href='/User/create'>Nuevo usuario</a> (se implementa en clase)</li>
         			</ul>
-        			<p>Estas operaciones se implementan en clase, están descritas en los apuntes.</p>
         		</section>
         		<?php } ?>
     		</div>
+    		
+    		
+    		<div class="flex-container gap2">
+        		
+        		<section class="flex2 p1">
+        			<h2 class="my2">Estadísticas del disco</h2>
+
+					<p>Estas estadísticas muestran el espacio total y disponible del 
+					disco o partición en la que se encuentra hospedada la aplicación.</p>
+					
+					
+					
+        			<ul class="my2">
+        				<?php 
+        				    // cálculos
+            				$espacioTotal   = disk_total_space('.');
+            				$espacioLibre   = disk_free_space('.');
+            				$espacioOcupado = $espacioTotal-$espacioLibre;
+            				$porcentaje     = $espacioOcupado/$espacioTotal*100;
+        				?>
+        				<li>
+        					<span class="inline-block w50">Espacio total en la partición:</span> 
+        					<b><?= round($espacioTotal/1024/1024/1024,2) ?></b> Gb
+        				</li>
+        				<li>
+        					<span class="inline-block w50">Espacio ocupado:</span>
+        					<b><?= round($espacioOcupado/1024/1024/1024,2) ?></b> Gb
+        				</li>	
+        				<li>
+        					<span class="inline-block w50">Espacio disponible:</span>
+        					<b><?= round($espacioLibre/1024/1024/1024,2) ?></b> Gb
+        				</li>
+        				<li>
+        					<span class="inline-block w50">Porcentaje de ocupación:</span>
+        					<b><?= floor($porcentaje) ?></b>%
+        				</li>				
+        			</ul>	
+        			
+        			<p class="caution">No corresponden con el espacio de usuario
+					disponible en hostings compartidos.</p>
+					
+					<p>El espacio disponible y total para la cuenta de usuario se
+					consulta desde el panel de control del hosting.</p>
+					
+        		</section>
+        		
+        		<figure class="flex1">
+        			<img src="/images/template/disk.png" alt="estadísticas del disco" title="estadísticas del disco">
+        		</figure>
+        	</div>
+        	
 		</main>
 		
 		<?= $template->footer() ?>
